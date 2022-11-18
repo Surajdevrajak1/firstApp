e0e32d7ff9de677a82c3c9c5e62aeb3972fbda64
sirisha <sireesha@onnetsystems.net>
2022-11-18 09:40:43 +0530

    first ever commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstcomponentComponent } from './component/firstcomponent/firstcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcomponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
